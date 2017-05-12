77aed3558eff332bcacda4f1a8e3056c6a157443
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2017-05-11 23:39:07 -0600

    Script for building game as exe file

---
+++ release.bat
@@ -0,0 +1,12 @@
+call build.bat
+cp "Tower Defense.love" "./../build/Tower Defense.love"
+cd ./../build
+mv "Tower Defense.love" TD.love
+cp ../love.exe love.exe
+copy /b love.exe + TD.love TD.exe
+mv TD.exe "Tower Defense.exe"
+rm TD.love
+rm love.exe
+set path="C:\Program Files\WinRAR\";%path%
+winrar a -r -afzip "Tower Defense"
+cd "./../Tower Defense"
