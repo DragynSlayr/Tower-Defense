b19ce8b91ccae66a09c417b1111b206666af3e0b
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2017-05-13 01:21:51 -0600

    Added mode, standardized enemy spawning and randomizing

--- logic/objectives.lua
+++ logic/objectives.lua
@@ -3,7 +3,7 @@ do
   local _base_0 = {
     nextMode = function(self)
       self.counter = self.counter + 1
-      if self.counter <= #self.modes then
+      if self.counter <= self.num_modes then
         self.mode = self.modes[self.counter]
         return self.mode:start()
       else
@@ -16,14 +16,27 @@ do
       return self.mode:entityKilled(entity)
     end,
     update = function(self, dt)
+      local start_difficulty = self.difficulty
       if not self.mode.complete then
-        return self.mode:update(dt)
+        self.mode:update(dt)
+        if self.mode.complete then
+          local score = SCORE + (self.difficulty * 1000)
+          SCORE = score
+        end
       else
         self.elapsed = self.elapsed + dt
         if self.elapsed >= self.delay then
           self.elapsed = 0
-          return self:nextMode()
+          self:nextMode()
         end
+      end
+      if start_difficulty ~= self.difficulty then
+        local factor = (self.difficulty - 1) * 0.02
+        self.basicChance = MathHelper:clamp(0.80 - factor, 0.20, 0.80)
+        self.playerChance = MathHelper:clamp(0.05 + (factor / 4), 0.05, 0.20)
+        self.turretChance = MathHelper:clamp(0.05 + (factor / 4), 0.05, 0.20)
+        self.strongChance = MathHelper:clamp(0.05 + (factor / 4), 0.05, 0.20)
+        self.spawnerChance = MathHelper:clamp(0.05 + (factor / 4), 0.05, 0.20)
       end
     end,
     draw = function(self)
@@ -34,19 +47,100 @@ do
         Renderer:drawStatusMessage("Objective Complete!", love.graphics.getHeight() / 2, Renderer.title_font)
         return love.graphics.pop()
       end
+    end,
+    getRandomEnemy = function(self, basicChance, playerChance, turretChance, strongChance, spawnerChance)
+      if basicChance == nil then
+        basicChance = self.basicChance
+      end
+      if playerChance == nil then
+        playerChance = self.playerChance
+      end
+      if turretChance == nil then
+        turretChance = self.turretChance
+      end
+      if strongChance == nil then
+        strongChance = self.strongChance
+      end
+      if spawnerChance == nil then
+        spawnerChance = self.spawnerChance
+      end
+      local num = math.random()
+      if num <= basicChance then
+        return EnemyTypes.basic, 1
+      elseif num <= basicChance + playerChance then
+        return EnemyTypes.player, 1
+      elseif num <= basicChance + playerChance + turretChance then
+        return EnemyTypes.turret, 1
+      elseif num <= basicChance + playerChance + turretChance + strongChance then
+        return EnemyTypes.strong, 1
+      else
+        return EnemyTypes.spawner, 5
+      end
+    end,
+    spawn = function(self, typeof, i)
+      if i == nil then
+        i = 0
+      end
+      local x = math.random(love.graphics.getWidth())
+      local y = math.random(love.graphics.getHeight())
+      local enemy
+      local _exp_0 = typeof
+      if EnemyTypes.player == _exp_0 then
+        enemy = PlayerEnemy(x, y)
+      elseif EnemyTypes.turret == _exp_0 then
+        enemy = TurretEnemy(x, y)
+      elseif EnemyTypes.spawner == _exp_0 then
+        enemy = SpawnerEnemy(x, y)
+      elseif EnemyTypes.strong == _exp_0 then
+        enemy = StrongEnemy(x, y)
+      elseif EnemyTypes.basic == _exp_0 then
+        enemy = BasicEnemy(x, y)
+      elseif GoalTypes.attack == _exp_0 then
+        enemy = AttackGoal(x, y)
+      elseif GoalTypes.defend == _exp_0 then
+        enemy = DefendGoal(x, y)
+      end
+      local touching = false
+      for k, v in pairs(Driver.objects) do
+        for k2, o in pairs(v) do
+          local object = o:getHitBox()
+          local e = enemy:getHitBox()
+          if object:contains(e) then
+            touching = true
+            break
+          end
+        end
+      end
+      if touching or not enemy:isOnScreen(20) then
+        return self:spawn(typeof, i + 1)
+      else
+        if typeof == GoalTypes.attack or typeof == GoalTypes.defend then
+          return Driver:addObject(enemy, EntityTypes.goal)
+        else
+          return Driver:addObject(enemy, EntityTypes.enemy)
+        end
+      end
     end
   }
   _base_0.__index = _base_0
   _class_0 = setmetatable({
     __init = function(self)
-      self.num_modes = 1
       self.mode = nil
       self.elapsed = 0
       self.delay = 10
       self.modes = {
-        EliminationMode()
+        AttackMode(self),
+        EliminationMode(self)
       }
+      self.num_modes = #self.modes
+      MathHelper:shuffle(self.modes)
       self.counter = 0
+      self.difficulty = 0
+      self.basicChance = 0.8
+      self.playerChance = 0.05
+      self.turretChance = 0.05
+      self.strongChance = 0.05
+      self.spawnerChance = 0.05
     end,
     __base = _base_0,
     __name = "Objectives"
--- logic/objectives.moon
+++ logic/objectives.moon
@@ -1,15 +1,25 @@
 export class Objectives
   new: =>
-    @num_modes = 1
     @mode = nil
     @elapsed = 0
     @delay = 10
-    @modes = {EliminationMode!}
+    @modes = {
+      AttackMode @,
+      EliminationMode @
+    }
+    @num_modes = #@modes
+    MathHelper\shuffle @modes
     @counter = 0
+    @difficulty = 0
+    @basicChance = 0.8
+    @playerChance = 0.05
+    @turretChance = 0.05
+    @strongChance = 0.05
+    @spawnerChance = 0.05
 
   nextMode: =>
     @counter += 1
-    if @counter <= #@modes
+    if @counter <= @num_modes
       @mode = @modes[@counter]
       @mode\start!
     else
@@ -22,13 +32,24 @@ export class Objectives
     @mode\entityKilled entity
 
   update: (dt) =>
+    start_difficulty = @difficulty
     if not @mode.complete
       @mode\update dt
+      if @mode.complete
+        score = SCORE + (@difficulty * 1000)
+        export SCORE = score
     else
       @elapsed += dt
       if @elapsed >= @delay
         @elapsed = 0
         @nextMode!
+    if start_difficulty ~= @difficulty
+      factor = (@difficulty - 1) * 0.02
+      @basicChance = MathHelper\clamp 0.80 - factor, 0.20, 0.80
+      @playerChance = MathHelper\clamp 0.05 + (factor / 4), 0.05, 0.20
+      @turretChance = MathHelper\clamp 0.05 + (factor / 4), 0.05, 0.20
+      @strongChance = MathHelper\clamp 0.05 + (factor / 4), 0.05, 0.20
+      @spawnerChance = MathHelper\clamp 0.05 + (factor / 4), 0.05, 0.20
 
   draw: =>
     if not @mode.complete
@@ -37,3 +58,50 @@ export class Objectives
       love.graphics.push "all"
       Renderer\drawStatusMessage "Objective Complete!", love.graphics.getHeight! / 2, Renderer.title_font
       love.graphics.pop!
+
+  getRandomEnemy: (basicChance = @basicChance, playerChance = @playerChance, turretChance = @turretChance, strongChance = @strongChance, spawnerChance = @spawnerChance) =>
+    num = math.random!
+    if num <= basicChance
+      return EnemyTypes.basic, 1
+    elseif num <= basicChance + playerChance
+      return EnemyTypes.player, 1
+    elseif num <= basicChance + playerChance + turretChance
+      return EnemyTypes.turret, 1
+    elseif num <= basicChance + playerChance + turretChance + strongChance
+      return EnemyTypes.strong, 1
+    else
+      return EnemyTypes.spawner, 5
+
+  spawn: (typeof, i = 0) =>
+    x = math.random love.graphics.getWidth!
+    y = math.random love.graphics.getHeight!
+    enemy = switch typeof
+      when EnemyTypes.player
+        PlayerEnemy x, y
+      when EnemyTypes.turret
+        TurretEnemy x, y
+      when EnemyTypes.spawner
+        SpawnerEnemy x, y
+      when EnemyTypes.strong
+        StrongEnemy x, y
+      when EnemyTypes.basic
+        BasicEnemy x, y
+      when GoalTypes.attack
+        AttackGoal x, y
+      when GoalTypes.defend
+        DefendGoal x, y
+    touching = false
+    for k, v in pairs Driver.objects
+      for k2, o in pairs v
+        object = o\getHitBox!
+        e = enemy\getHitBox!
+        if object\contains e
+          touching = true
+          break
+    if touching or not enemy\isOnScreen 20
+      @spawn typeof, i + 1
+    else
+      if typeof == GoalTypes.attack or typeof == GoalTypes.defend
+        Driver\addObject enemy, EntityTypes.goal
+      else
+        Driver\addObject enemy, EntityTypes.enemy
