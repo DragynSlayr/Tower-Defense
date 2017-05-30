1a0ae3d1b9c81cda15783018c06e3836c98651b0
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2017-05-29 23:22:46 -0600

    Added size parameter

--- shaders/distance.fs
+++ shaders/distance.fs
@@ -1,11 +1,12 @@
 extern vec2 screen_size;
 extern vec2 player_pos;
+extern float size = 10; // increase to decrease radius
 
 vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
   float dist = length(screen_size);
   vec2 sep = screen_coords - player_pos;
   float num = length(sep) / dist;
-  num *= 5;
+  num *= size;
   num = clamp(1.0 - num, 0, 1);
 
   vec4 pixel = Texel(texture, texture_coords);
