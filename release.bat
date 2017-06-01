9332b162b602703233b5b9f433989808d4121cd7
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2017-06-01 02:59:32 -0600

    Build script creates Windows and Mac executable

--- release.bat
+++ release.bat
@@ -5,8 +5,10 @@ mv "Tower Defense.love" TD.love
 cp ../love.exe love.exe
 copy /b love.exe + TD.love TD.exe
 mv TD.exe "Tower Defense.exe"
-rm TD.love
+mv TD.love "./../release/Mac/Tower Defense.love"
 rm love.exe
+cp * ./../release/Windows
+cd ./../release
 set path="C:\Program Files\WinRAR\";%path%
 winrar a -r -afzip "Tower Defense"
 cd "./../Tower Defense"
