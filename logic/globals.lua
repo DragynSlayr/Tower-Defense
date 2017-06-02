688c925584603966dc487ac5666aa4ad998499b2
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2017-06-02 14:58:00 -0600

    Added function for reloading stats

--- logic/globals.lua
+++ logic/globals.lua
@@ -1,10 +1,12 @@
 Stats = { }
-for k, v in pairs(Base_Stats) do
-  Stats[k] = { }
-  local count = 1
-  for k2, v2 in pairs(v) do
-    Stats[k][k2] = v2
-    Stats[k][count] = v2
-    count = count + 1
+loadBaseStats = function()
+  for k, v in pairs(Base_Stats) do
+    Stats[k] = { }
+    local count = 1
+    for k2, v2 in pairs(v) do
+      Stats[k][k2] = v2
+      Stats[k][count] = v2
+      count = count + 1
+    end
   end
 end
--- logic/globals.moon
+++ logic/globals.moon
@@ -1,9 +1,10 @@
 export Stats = {}
 
-for k, v in pairs Base_Stats
-  Stats[k] = {}
-  count = 1
-  for k2, v2 in pairs v
-    Stats[k][k2] = v2
-    Stats[k][count] = v2
-    count += 1
+export loadBaseStats = () ->
+  for k, v in pairs Base_Stats
+    Stats[k] = {}
+    count = 1
+    for k2, v2 in pairs v
+      Stats[k][k2] = v2
+      Stats[k][count] = v2
+      count += 1
