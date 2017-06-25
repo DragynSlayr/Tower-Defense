5017ca690e6abbf8d13e02f9c3f05e896d68ad40
DragynSlayr <inderpreet_dhillon@hotmail.com>
2017-06-25 03:39:04 -0600

    Basic loading screen

--- main.lua
+++ main.lua
@@ -1,5 +1,28 @@
 require("logic.constants")
 require("logic.globals")
-require("logic.classLoader")
-VERSION = "V.11B"
-Driver = Driver()
+local queue = { }
+local size = 0
+local elements = 0
+love.update = function(dt)
+  if #queue > 0 then
+    require(table.remove(queue, 1))
+    size = Screen_Size.width * (1 - (#queue / elements))
+  else
+    queue = nil
+    Driver = Driver()
+    return Driver.load()
+  end
+end
+love.draw = function()
+  love.graphics.push("all")
+  love.graphics.setColor(0, 0, 0, 255)
+  love.graphics.rectangle("fill", 0, 0, Screen_Size.width, Screen_Size.height)
+  love.graphics.setColor(75, 163, 255, 255)
+  love.graphics.rectangle("fill", (Screen_Size.width - size) / 2, 0, size, Screen_Size.height)
+  return love.graphics.pop()
+end
+love.load = function()
+  queue = require("logic.classLoader")
+  elements = #queue
+  VERSION = "V.11B"
+end
--- main.moon
+++ main.moon
@@ -1,8 +1,28 @@
 require "logic.constants"
 require "logic.globals"
-require "logic.classLoader"
 
-export VERSION = "V.11B"
+queue = {}
+size = 0
+elements = 0
 
--- Create Driver object
-export Driver = Driver!
+love.update = (dt) ->
+  if #queue > 0
+    require table.remove queue, 1
+    size = Screen_Size.width * (1 - (#queue / elements))
+  else
+    queue = nil
+    export Driver = Driver!
+    Driver.load!
+
+love.draw = () ->
+  love.graphics.push "all"
+  love.graphics.setColor 0, 0, 0, 255
+  love.graphics.rectangle "fill", 0, 0, Screen_Size.width, Screen_Size.height
+  love.graphics.setColor 75, 163, 255, 255
+  love.graphics.rectangle "fill", (Screen_Size.width - size) / 2, 0, size, Screen_Size.height
+  love.graphics.pop!
+
+love.load = () ->
+  queue = require "logic.classLoader"
+  elements = #queue
+  export VERSION = "V.11B"
