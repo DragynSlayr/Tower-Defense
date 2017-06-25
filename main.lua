d4810a6cedb80d3a198fa036cb1583978b01ca11
DragynSlayr <inderpreet_dhillon@hotmail.com>
2017-06-25 12:16:06 -0600

    Smoother loading, with percentage indicator

--- main.lua
+++ main.lua
@@ -3,14 +3,20 @@ require("logic.globals")
 local queue = { }
 local size = 0
 local elements = 0
+local max_size = 0
+local speed = Screen_Size.width / 3
 love.update = function(dt)
-  if #queue > 0 then
-    require(table.remove(queue, 1))
-    size = Screen_Size.width * (1 - (#queue / elements))
-  else
-    queue = nil
-    Driver = Driver()
-    return Driver.load()
+  size = size + (speed * dt)
+  if size >= max_size then
+    size = max_size
+    if #queue > 0 then
+      require(table.remove(queue, 1))
+      max_size = Screen_Size.width * (1 - (#queue / elements))
+    else
+      queue = nil
+      Driver = Driver()
+      return Driver.load()
+    end
   end
 end
 love.draw = function()
@@ -19,10 +25,14 @@ love.draw = function()
   love.graphics.rectangle("fill", 0, 0, Screen_Size.width, Screen_Size.height)
   love.graphics.setColor(75, 163, 255, 255)
   love.graphics.rectangle("fill", (Screen_Size.width - size) / 2, 0, size, Screen_Size.height)
+  love.graphics.setColor(255, 255, 255, 255)
+  local text = size / Screen_Size.width
+  text = math.floor(text * 100)
+  love.graphics.printf("Loading " .. text .. "%\t", 0, Screen_Size.height - (25 * Scale.height), Screen_Size.width, "right")
   return love.graphics.pop()
 end
 love.load = function()
   queue = require("logic.classLoader")
   elements = #queue
-  VERSION = "V.11B"
+  VERSION = "V.12"
 end
--- main.moon
+++ main.moon
@@ -4,15 +4,20 @@ require "logic.globals"
 queue = {}
 size = 0
 elements = 0
+max_size = 0
+speed = Screen_Size.width / 3
 
 love.update = (dt) ->
-  if #queue > 0
-    require table.remove queue, 1
-    size = Screen_Size.width * (1 - (#queue / elements))
-  else
-    queue = nil
-    export Driver = Driver!
-    Driver.load!
+  size += speed * dt
+  if size >= max_size
+    size = max_size
+    if #queue > 0
+      require table.remove queue, 1
+      max_size = Screen_Size.width * (1 - (#queue / elements))
+    else
+      queue = nil
+      export Driver = Driver!
+      Driver.load!
 
 love.draw = () ->
   love.graphics.push "all"
@@ -20,9 +25,13 @@ love.draw = () ->
   love.graphics.rectangle "fill", 0, 0, Screen_Size.width, Screen_Size.height
   love.graphics.setColor 75, 163, 255, 255
   love.graphics.rectangle "fill", (Screen_Size.width - size) / 2, 0, size, Screen_Size.height
+  love.graphics.setColor 255, 255, 255, 255
+  text = size / Screen_Size.width
+  text = math.floor text * 100
+  love.graphics.printf "Loading " .. text .. "%\t", 0, Screen_Size.height - (25 * Scale.height), Screen_Size.width, "right"
   love.graphics.pop!
 
 love.load = () ->
   queue = require "logic.classLoader"
   elements = #queue
-  export VERSION = "V.11B"
+  export VERSION = "V.12"
