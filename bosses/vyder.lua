567016d1a4c35d51e452e34c05d982a7167c9d10
DragynSlayr <inderpreet_dhillon@hotmail.com>
2017-06-25 20:44:42 -0600

    Added AI

--- bosses/vyder.lua
+++ bosses/vyder.lua
@@ -8,13 +8,73 @@ do
       return Circle(self.position.x, self.position.y + (25 * Scale.height), radius)
     end,
     update = function(self, dt)
-      self.speed = Vector(self.target_position.x - self.position.x, self.target_position.y - self.position.y)
-      local dist = self.speed:getLength()
-      self.speed:toUnitVector()
-      self.speed = self.speed:multiply(self.speed_multiplier)
-      _class_0.__parent.__base.update(self, dt)
-      if dist <= self:getHitBox().radius then
-        self.target_position = Driver:getRandomPosition()
+      self.ai_time = self.ai_time + dt
+      local _exp_0 = self.ai_phase
+      if 1 == _exp_0 then
+        self.speed = Vector(self.target_position.x - self.position.x, self.target_position.y - self.position.y)
+        local dist = self.speed:getLength()
+        self.speed:toUnitVector()
+        self.speed = self.speed:multiply(self.speed_multiplier)
+        _class_0.__parent.__base.update(self, dt)
+        self.chase_time = self.chase_time + dt
+        if dist <= self:getHitBox().radius or self.chase_time >= 3 then
+          self.chase_time = 0
+          self.target_position = Driver:getRandomPosition()
+        end
+        if self.ai_time >= 10 then
+          self.ai_time = 0
+          self.ai_phase = self.ai_phase + 1
+        end
+      elseif 2 == _exp_0 then
+        local targets = { }
+        if Driver.objects[EntityTypes.player] then
+          targets = concatTables(targets, Driver.objects[EntityTypes.player])
+        end
+        if Driver.objects[EntityTypes.turret] then
+          targets = concatTables(targets, Driver.objects[EntityTypes.turret])
+        end
+        local target = pick(targets)
+        self.target_position = target.position
+        self.ai_time = 0
+        self.ai_phase = self.ai_phase + 1
+      elseif 3 == _exp_0 then
+        self.speed = Vector(self.target_position.x - self.position.x, self.target_position.y - self.position.y)
+        local dist = self.speed:getLength()
+        self.speed:toUnitVector()
+        self.speed = self.speed:multiply(self.speed_multiplier * self.boost_multiplier)
+        _class_0.__parent.__base.update(self, dt)
+        self.chase_time = self.chase_time + dt
+        if dist < self:getHitBox().radius + (self.attack_range / 2) or self.ai_time >= 5 then
+          self.ai_time = 0
+          self.ai_phase = self.ai_phase + 1
+        end
+      elseif 4 == _exp_0 then
+        self.speed = Vector(0, 0)
+        self.sprite = self.action_sprite
+        self.ai_time = 0
+        self.ai_phase = self.ai_phase + 1
+      elseif 5 == _exp_0 then
+        _class_0.__parent.__base.update(self, dt)
+        if Driver.objects[EntityTypes.player] then
+          for k, v in pairs(Driver.objects[EntityTypes.player]) do
+            local player = v:getHitBox()
+            local boss = self:getHitBox()
+            boss.radius = boss.radius + self.attack_range
+            if player:contains(boss) then
+              v:onCollide(self)
+            end
+          end
+        end
+        if Driver.objects[EntityTypes.turret] then
+          for k, v in pairs(Driver.objects[EntityTypes.turret]) do
+            local turret = v:getAttackHitBox()
+            local boss = self:getHitBox()
+            boss.radius = boss.radius + self.attack_range
+            if turret:contains(boss) then
+              v:onCollide(self)
+            end
+          end
+        end
       end
     end,
     draw = function(self)
@@ -38,12 +98,29 @@ do
       self.health = 300
       self.max_health = self.health
       self.speed_multiplier = 200
-      sprite = Sprite("poison.tga", 64, 64, 1, 2)
-      self.trail = ParticleTrail(self.position.x, self.position.y, sprite, self)
-      self.trail.life_time = 3
-      self.ai = { }
-      self.ai.phase = 1
+      self.boost_multiplier = 3
+      self.chase_time = 0
+      self.damage = 5 / 60
+      self.attack_range = 100 * Scale.diag
+      sprite = Sprite("poison.tga", 64, 64, 1, 1.75)
+      self.trail = ParticleEmitter(self.position.x, self.position.y, 0.2, 3, self)
+      self.trail.sprite = sprite
+      self.trail.particle_type = ParticleTypes.poison
+      self.trail.moving_particles = false
+      self.ai_phase = 1
+      self.ai_time = 0
       self.target_position = Driver:getRandomPosition()
+      local splitted = split(self.normal_sprite.name, ".")
+      local name = splitted[1] .. "Action." .. splitted[2]
+      local height, width, _, scale = self.normal_sprite:getProperties()
+      self.action_sprite = ActionSprite(name, height, width, 0.5, 1, self, function(self)
+        self.parent.ai_phase = 1
+        self.parent.ai_time = 0
+        self.parent.target_position = Driver:getRandomPosition()
+      end)
+      local x_scale = self.normal_sprite.scaled_width / (self.normal_sprite.width * Scale.width)
+      local y_scale = self.normal_sprite.scaled_height / (self.normal_sprite.height * Scale.height)
+      return self.action_sprite:setScale(x_scale, y_scale)
     end,
     __base = _base_0,
     __name = "BossVyder",
--- bosses/vyder.moon
+++ bosses/vyder.moon
@@ -7,14 +7,37 @@ export class BossVyder extends Boss
     @health = 300
     @max_health = @health
     @speed_multiplier = 200
+    @boost_multiplier = 3
+    @chase_time = 0
+    @damage = 5 / 60
+    @attack_range = 100 * Scale.diag
 
-    sprite = Sprite "poison.tga", 64, 64, 1, 2
-    @trail = ParticleTrail @position.x, @position.y, sprite, @
-    @trail.life_time = 3
+    sprite = Sprite "poison.tga", 64, 64, 1, 1.75
+    @trail = ParticleEmitter @position.x, @position.y, 0.2, 3, @
+    --@trail = ParticleTrail @position.x, @position.y, sprite, @
+    --@trail.life_time = 3
+    @trail.sprite = sprite
+    @trail.particle_type = ParticleTypes.poison
+    @trail.moving_particles = false
 
-    @ai = {}
-    @ai.phase = 1
+    @ai_phase = 1
+    @ai_time = 0
     @target_position = Driver\getRandomPosition!
+
+    splitted = split @normal_sprite.name, "."
+    name = splitted[1] .. "Action." .. splitted[2]
+    height, width, _, scale = @normal_sprite\getProperties!
+
+    @action_sprite = ActionSprite name, height, width, 0.5, 1, @, () =>
+      @parent.ai_phase = 1
+      @parent.ai_time = 0
+      @parent.target_position = Driver\getRandomPosition!
+
+    x_scale = @normal_sprite.scaled_width / (@normal_sprite.width * Scale.width)
+    y_scale = @normal_sprite.scaled_height / (@normal_sprite.height * Scale.height)
+
+    @action_sprite\setScale x_scale, y_scale
+
 
   getHitBox: =>
     -- Get the radius of this Sprite as the minimum of height and width
@@ -25,13 +48,62 @@ export class BossVyder extends Boss
     return Circle @position.x, @position.y + (25 * Scale.height), radius
 
   update: (dt) =>
-    @speed = Vector @target_position.x - @position.x, @target_position.y - @position.y
-    dist = @speed\getLength!
-    @speed\toUnitVector!
-    @speed = @speed\multiply @speed_multiplier
-    super dt
-    if dist <= @getHitBox!.radius
-      @target_position = Driver\getRandomPosition!
+    @ai_time += dt
+    switch @ai_phase
+      when 1
+        @speed = Vector @target_position.x - @position.x, @target_position.y - @position.y
+        dist = @speed\getLength!
+        @speed\toUnitVector!
+        @speed = @speed\multiply @speed_multiplier
+        super dt
+        @chase_time += dt
+        if dist <= @getHitBox!.radius or @chase_time >= 3
+          @chase_time = 0
+          @target_position = Driver\getRandomPosition!
+        if @ai_time >= 10
+          @ai_time = 0
+          @ai_phase += 1
+      when 2
+        targets = {}
+        if Driver.objects[EntityTypes.player]
+          targets = concatTables targets, Driver.objects[EntityTypes.player]
+        if Driver.objects[EntityTypes.turret]
+          targets = concatTables targets, Driver.objects[EntityTypes.turret]
+        target = pick targets
+        @target_position = target.position
+        @ai_time = 0
+        @ai_phase += 1
+      when 3
+        @speed = Vector @target_position.x - @position.x, @target_position.y - @position.y
+        dist = @speed\getLength!
+        @speed\toUnitVector!
+        @speed = @speed\multiply @speed_multiplier * @boost_multiplier
+        super dt
+        @chase_time += dt
+        if dist < @getHitBox!.radius + (@attack_range / 2) or @ai_time >= 5
+          @ai_time = 0
+          @ai_phase += 1
+      when 4
+        @speed = Vector 0, 0
+        @sprite = @action_sprite
+        @ai_time = 0
+        @ai_phase += 1
+      when 5
+        super dt
+        if Driver.objects[EntityTypes.player]
+          for k, v in pairs Driver.objects[EntityTypes.player]
+            player = v\getHitBox!
+            boss = @getHitBox!
+            boss.radius += @attack_range
+            if player\contains boss
+              v\onCollide @
+        if Driver.objects[EntityTypes.turret]
+          for k, v in pairs Driver.objects[EntityTypes.turret]
+            turret = v\getAttackHitBox!
+            boss = @getHitBox!
+            boss.radius += @attack_range
+            if turret\contains boss
+              v\onCollide @
 
   draw: =>
     super!
