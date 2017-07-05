961cbfc9aaf8129294cc05fe0bda44eecfe3bba3
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2017-07-05 12:22:57 -0600

    Removed enemy stats, shows items

--- screens/pause.lua
+++ screens/pause.lua
@@ -2,50 +2,38 @@ do
   local _class_0
   local _parent_0 = Screen
   local _base_0 = {
-    getEnemies = function(self)
-      local basic = BasicEnemy(0, 0)
-      local player = PlayerEnemy(0, 0)
-      local spawner = SpawnerEnemy(0, 0)
-      local strong = StrongEnemy(0, 0)
-      local turret = TurretEnemy(0, 0)
-      return {
-        basic,
-        player,
-        spawner,
-        strong,
-        turret
-      }
+    update = function(self, dt)
+      local typeof = ItemFrame
+      local frames = UI:filter(typeof, Screen_State.inventory)
+      local frames2 = UI:filter(typeof)
+      for k, v in pairs(frames2) do
+        v:setItem(frames[k].item)
+        v.sprite:setScale(2.5)
+        v.small_sprite = v.sprite
+        v.normal_sprite = v.small_sprite
+      end
     end,
     draw = function(self)
       love.graphics.push("all")
       for j = 1, 2 do
         local y = Screen_Size.height * 0.4
         local stats = Stats.player
-        local x = Screen_Size.width * 0.2
+        local x = Screen_Size.width * 0.05
         local icons = self.player_icons
         if j == 2 then
           stats = Stats.turret
-          x = Screen_Size.width * 0.8
+          x = Screen_Size.width * 0.90
           icons = self.turret_icons
         end
-        local bounds = self.sprites2[j]:getBounds(x, y)
+        local bounds = self.sprites[j]:getBounds(x, y)
         for i = 1, #stats do
           y = map(i, 1, #stats, (Screen_Size.height * 0.4) + (40 * Scale.height), Screen_Size.height * 0.60)
           icons[i]:draw(x, y + (9 * Scale.height))
           Renderer:drawHUDMessage((string.format("%.2f", stats[i])), x + (10 * Scale.width) + bounds.radius, y, self.font)
         end
       end
-      local enemies = self:getEnemies()
-      for i = 1, #enemies do
-        local x = map(i, 1, #enemies, 100 * Scale.width, Screen_Size.width - (200 * Scale.width))
-        local y = Screen_Size.height * 0.8
-        local bounds = self.sprites[i]:getBounds(x, y)
-        for j = 1, #self.stats do
-          y = map(j, 1, #self.stats, (Screen_Size.height * 0.8) + (30 * Scale.height), Screen_Size.height * 0.96)
-          self.icons[j]:draw(x, y + (9 * Scale.height))
-          Renderer:drawHUDMessage((string.format("%.2f", enemies[i][self.stats[j]])), x + (10 * Scale.width) + bounds.radius, y, self.font)
-        end
-      end
+      Renderer:drawAlignedMessage(Inventory.message1, Screen_Size.height * 0.85, "center", Renderer.hud_font)
+      Renderer:drawAlignedMessage(Inventory.message2, Screen_Size.height * 0.89, "center", Renderer.hud_font)
       return love.graphics.pop()
     end
   }
@@ -62,35 +50,23 @@ do
         "score_value"
       }
       self.sprites = {
-        (Sprite("enemy/tracker.tga", 32, 32, 1, 50 / 32)),
-        (Sprite("enemy/enemy.tga", 26, 26, 1, 50 / 26)),
-        (Sprite("projectile/dart.tga", 17, 17, 1, 50 / 17)),
-        (Sprite("enemy/bullet.tga", 26, 20, 1, 50 / 26)),
-        (Sprite("enemy/circle.tga", 26, 26, 1, 50 / 26))
-      }
-      self.sprites2 = {
-        (Sprite("test.tga", 16, 16, 0.29, 50 / 16)),
-        (Sprite("turret.tga", 34, 16, 2, 50 / 34))
+        (Sprite("player/test.tga", 16, 16, 0.29, 50 / 16)),
+        (Sprite("turret/turret.tga", 34, 16, 2, 50 / 34))
       }
       self.icons = {
-        (Sprite("icons/health.tga", 16, 16, 1, 1)),
-        (Sprite("icons/damage.tga", 16, 16, 1, 1)),
-        (Sprite("icons/speed.tga", 16, 16, 1, 1)),
-        (Sprite("icons/score.tga", 16, 16, 1, 1))
+        (Sprite("ui/icons/health.tga", 16, 16, 1, 1)),
+        (Sprite("ui/icons/range.tga", 16, 16, 1, 1)),
+        (Sprite("ui/icons/damage.tga", 16, 16, 1, 1)),
+        (Sprite("ui/icons/speed.tga", 16, 16, 1, 1)),
+        (Sprite("ui/icons/attack_delay.tga", 16, 16, 1, 1))
       }
-      self.player_icons = {
-        self.icons[1],
-        (Sprite("icons/range.tga", 16, 16, 1, 1)),
-        self.icons[2],
-        self.icons[3],
-        (Sprite("icons/attack_delay.tga", 16, 16, 1, 1))
-      }
+      self.player_icons = self.icons
       self.turret_icons = {
         self.icons[1],
-        (Sprite("icons/range.tga", 16, 16, 1, 1)),
         self.icons[2],
-        (Sprite("icons/cooldown.tga", 16, 16, 1, 1)),
-        (Sprite("icons/attack_delay.tga", 16, 16, 1, 1))
+        self.icons[3],
+        (Sprite("ui/icons/cooldown.tga", 16, 16, 1, 1)),
+        self.icons[5]
       }
     end,
     __base = _base_0,
--- screens/pause.moon
+++ screens/pause.moon
@@ -3,44 +3,43 @@ export class PauseScreen extends Screen
     super!
     @font = Renderer\newFont 20
     @stats = {"max_health", "damage", "speed_multiplier", "score_value"}
-    @sprites = {(Sprite "enemy/tracker.tga", 32, 32, 1, 50 / 32), (Sprite "enemy/enemy.tga", 26, 26, 1, 50 / 26), (Sprite "projectile/dart.tga", 17, 17, 1, 50 / 17), (Sprite "enemy/bullet.tga", 26, 20, 1, 50 / 26), (Sprite "enemy/circle.tga", 26, 26, 1, 50 / 26)}
-    @sprites2 = {(Sprite "test.tga", 16, 16, 0.29, 50 / 16), (Sprite "turret.tga", 34, 16, 2, 50 / 34)}
-    @icons = {(Sprite "icons/health.tga", 16, 16, 1, 1), (Sprite "icons/damage.tga", 16, 16, 1, 1), (Sprite "icons/speed.tga", 16, 16, 1, 1), (Sprite "icons/score.tga", 16, 16, 1, 1)}
-    @player_icons = {@icons[1], (Sprite "icons/range.tga", 16, 16, 1, 1), @icons[2], @icons[3], (Sprite "icons/attack_delay.tga", 16, 16, 1, 1)}
-    @turret_icons = {@icons[1], (Sprite "icons/range.tga", 16, 16, 1, 1), @icons[2], (Sprite "icons/cooldown.tga", 16, 16, 1, 1), (Sprite "icons/attack_delay.tga", 16, 16, 1, 1)}
+    @sprites = {(Sprite "player/test.tga", 16, 16, 0.29, 50 / 16), (Sprite "turret/turret.tga", 34, 16, 2, 50 / 34)}
+    @icons = {
+      (Sprite "ui/icons/health.tga", 16, 16, 1, 1),
+      (Sprite "ui/icons/range.tga", 16, 16, 1, 1),
+      (Sprite "ui/icons/damage.tga", 16, 16, 1, 1),
+      (Sprite "ui/icons/speed.tga", 16, 16, 1, 1),
+      (Sprite "ui/icons/attack_delay.tga", 16, 16, 1, 1)
+    }
+    @player_icons = @icons
+    @turret_icons = {@icons[1], @icons[2], @icons[3], (Sprite "ui/icons/cooldown.tga", 16, 16, 1, 1), @icons[5]}
 
-  getEnemies: () =>
-    basic   = BasicEnemy 0, 0
-    player  = PlayerEnemy 0, 0
-    spawner = SpawnerEnemy 0, 0
-    strong  = StrongEnemy 0, 0
-    turret  = TurretEnemy 0, 0
-    return {basic, player, spawner, strong, turret}
+  update: (dt) =>
+    typeof = ItemFrame
+    frames = UI\filter typeof, Screen_State.inventory
+    frames2 = UI\filter typeof
+    for k, v in pairs frames2
+      v\setItem frames[k].item
+      v.sprite\setScale 2.5
+      v.small_sprite = v.sprite
+      v.normal_sprite = v.small_sprite
 
   draw: =>
     love.graphics.push "all"
     for j = 1, 2
       y = Screen_Size.height * 0.4
       stats = Stats.player
-      x = Screen_Size.width * 0.2
+      x = Screen_Size.width * 0.05
       icons = @player_icons
       if j == 2
         stats = Stats.turret
-        x = Screen_Size.width * 0.8
+        x = Screen_Size.width * 0.90
         icons = @turret_icons
-      bounds = @sprites2[j]\getBounds x, y
+      bounds = @sprites[j]\getBounds x, y
       for i = 1, #stats
         y = map i, 1, #stats, (Screen_Size.height * 0.4) + (40 * Scale.height), Screen_Size.height * 0.60
         icons[i]\draw x, y + (9 * Scale.height)
         Renderer\drawHUDMessage (string.format "%.2f", stats[i]), x + (10 * Scale.width) + bounds.radius, y, @font
-    enemies = @getEnemies!
-    for i = 1, #enemies
-      x = map i, 1, #enemies, 100 * Scale.width, Screen_Size.width - (200 * Scale.width)
-      y = Screen_Size.height * 0.8
-      bounds = @sprites[i]\getBounds x, y
-      for j = 1, #@stats
-        --y += 30
-        y = map j, 1, #@stats, (Screen_Size.height * 0.8) + (30 * Scale.height), Screen_Size.height * 0.96
-        @icons[j]\draw x, y + (9 * Scale.height)
-        Renderer\drawHUDMessage (string.format "%.2f", enemies[i][@stats[j]]), x + (10 * Scale.width) + bounds.radius, y, @font
+    Renderer\drawAlignedMessage Inventory.message1, Screen_Size.height * 0.85, "center", Renderer.hud_font
+    Renderer\drawAlignedMessage Inventory.message2, Screen_Size.height * 0.89, "center", Renderer.hud_font
     love.graphics.pop!
