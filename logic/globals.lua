dd02ed4a8292a7487b50c075ad05644c40570f85
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2017-07-12 01:03:22 -0600

    simplified copying stats

--- logic/globals.lua
+++ logic/globals.lua
@@ -1,12 +1,6 @@
 Stats = { }
+Stats_Dynamic = { }
 loadBaseStats = function()
-  for k, v in pairs(Base_Stats) do
-    Stats[k] = { }
-    local count = 1
-    for k2, v2 in pairs(v) do
-      Stats[k][k2] = v2
-      Stats[k][count] = v2
-      count = count + 1
-    end
-  end
+  Stats = copyTable(Base_Stats)
+  Stats_Dynamic = copyTable(Base_Stats)
 end
--- logic/globals.moon
+++ logic/globals.moon
@@ -1,10 +1,6 @@
 export Stats = {}
+export Stats_Dynamic = {}
 
 export loadBaseStats = () ->
-  for k, v in pairs Base_Stats
-    Stats[k] = {}
-    count = 1
-    for k2, v2 in pairs v
-      Stats[k][k2] = v2
-      Stats[k][count] = v2
-      count += 1
+  Stats = copyTable Base_Stats
+  Stats_Dynamic = copyTable Base_Stats
