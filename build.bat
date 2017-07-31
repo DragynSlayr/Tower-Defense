f389ae5acaa868768e5b585f1b56f3e8bebf7584
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2017-07-30 21:22:59 -0600

    Leaves out some files

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 rm "Tower Defense.love"
 call compile.bat
 set path="C:\Program Files\WinRAR\";%path%
-winrar a -r -x\.git -afzip "Tower Defense"
+winrar a -r -x\.git -x\*.bat -x\*.moon -x\*.py -x\changes.txt -afzip "Tower Defense"
 mv "Tower Defense.zip" "Tower Defense.love"
