580f2d752809126ba9380f3309c18709765fd125
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2018-08-31 09:40:35 -0600

    Works with windows now

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@ rm "Tower Defense.love"
 call compile.bat
 set path="C:\Program Files\WinRAR\";%path%
 winrar a -r -x\.git -x\*.bat -x\*.moon -x\*.py -x\changes.txt -afzip "Tower Defense"
-mv "Tower Defense.zip" "Tower Defense.love"
+move "Tower Defense.zip" "Tower Defense.love"
